90eb2cff93069f6c1af6137fbd83d11d56d05800
1vas1k <ivan.lyoskin@gmail.com>
2024-12-18 02:23:14 +0200

    #lab4 feat: complete labwork4 moved PictoreBoxes using keyboard and mouse, add report

---
+++ WindowsFormsApp4/UserInteractionForm.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace WindowsFormsApp4
+{
+    partial class UserInteractionForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UserInteractionForm));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.moveButton = new System.Windows.Forms.Button();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.InfMoveButton = new System.Windows.Forms.Button();
+            this.openButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WindowsFormsApp4.Properties.Resources.mista;
+            this.pictureBox1.Location = new System.Drawing.Point(100, 100);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // moveButton
+            // 
+            this.moveButton.Location = new System.Drawing.Point(12, 12);
+            this.moveButton.Name = "moveButton";
+            this.moveButton.Size = new System.Drawing.Size(151, 38);
+            this.moveButton.TabIndex = 1;
+            this.moveButton.Text = "Move image";
+            this.moveButton.UseVisualStyleBackColor = true;
+            this.moveButton.Click += new System.EventHandler(this.moveButton_Click);
+            // 
+            // resetButton
+            // 
+            this.resetButton.Location = new System.Drawing.Point(169, 12);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(151, 38);
+            this.resetButton.TabIndex = 2;
+            this.resetButton.Text = "Reset position";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // InfMoveButton
+            // 
+            this.InfMoveButton.Location = new System.Drawing.Point(326, 12);
+            this.InfMoveButton.Name = "InfMoveButton";
+            this.InfMoveButton.Size = new System.Drawing.Size(151, 38);
+            this.InfMoveButton.TabIndex = 3;
+            this.InfMoveButton.Text = "Ininity move";
+            this.InfMoveButton.UseVisualStyleBackColor = true;
+            this.InfMoveButton.Click += new System.EventHandler(this.InfMoveButton_Click);
+            // 
+            // openButton
+            // 
+            this.openButton.Location = new System.Drawing.Point(483, 12);
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(151, 38);
+            this.openButton.TabIndex = 4;
+            this.openButton.Text = "Next form";
+            this.openButton.UseVisualStyleBackColor = true;
+            this.openButton.Click += new System.EventHandler(this.openButton_Click);
+            // 
+            // UserInteractionForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(782, 553);
+            this.Controls.Add(this.openButton);
+            this.Controls.Add(this.InfMoveButton);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.moveButton);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "UserInteractionForm";
+            this.Text = "UserInteractionForm";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button moveButton;
+        private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.Button InfMoveButton;
+        private System.Windows.Forms.Button openButton;
+    }
+}
+
---
+++ WindowsFormsApp4/UserInteractionForm.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace WindowsFormsApp4
+{
+    public partial class UserInteractionForm : Form
+    {
+        public UserInteractionForm()
+        {
+            InitializeComponent();
+            this.ClientSize = new Size(800, 600);
+
+            // create obstacle
+            obstacle1 = new Panel
+            {
+                BackColor = Color.Red,
+                Size = new Size(70, 70),
+                Location = new Point(300, 150)
+            };
+            // add obstacle on the form
+            this.Controls.Add(obstacle1);
+            initialImagePosition = new Point(100, 100);
+
+        }
+
+        private Panel obstacle1;
+        private Point initialImagePosition;
+        Timer moveTimer = new Timer();
+        int xSpeed = 20;
+        int ySpeed = 20;
+
+        // move PictureBox on press the button
+        private void moveButton_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Location.X >= ClientSize.Width - pictureBox1.Width || pictureBox1.Location.X <= 0)
+            {
+                xSpeed = -xSpeed;
+            }
+            if (pictureBox1.Location.Y >= ClientSize.Height - pictureBox1.Height || pictureBox1.Location.Y <= 50)
+            {
+                ySpeed = -ySpeed;
+            }
+
+
+
+            pictureBox1.Location = new Point(pictureBox1.Location.X + xSpeed, pictureBox1.Location.Y + ySpeed);
+        }
+
+        // set PictureBox on start position
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            moveTimer.Stop();
+            pictureBox1.Location = initialImagePosition;
+            xSpeed = Math.Abs(xSpeed);
+            ySpeed = Math.Abs(ySpeed);
+        }
+
+
+        // start infinity move of PictureBox with x and y speed
+        private void InfMoveButton_Click(object sender, EventArgs e)
+        {
+            moveTimer.Tick += (s, eventArgs) =>
+            {
+
+                if (IsColliding(pictureBox1, obstacle1))
+                {
+                    if (pictureBox1.Location.X < obstacle1.Location.X)
+                    {
+                        xSpeed = -Math.Abs(xSpeed);  // change vector of movement if met right side of obstacle
+                    }
+                    else
+                    {
+                        xSpeed = Math.Abs(xSpeed);   // change vector of movement if met left side of obstacle
+                    }
+
+                    if (pictureBox1.Location.Y < obstacle1.Location.Y)
+                    {
+                        ySpeed = -Math.Abs(ySpeed);  // change vector of movement if met top side of obstacle
+                    }
+                    else
+                    {
+                        ySpeed = Math.Abs(ySpeed);   // change vector of movement if met bottom side of obstacle
+                    }
+                }
+
+                // change vector of movement if met one of the sides
+                if (pictureBox1.Location.X >= ClientSize.Width - pictureBox1.Width || pictureBox1.Location.X <= 0)
+                {
+                    xSpeed = -xSpeed;
+                }
+                if (pictureBox1.Location.Y >= ClientSize.Height - pictureBox1.Height || pictureBox1.Location.Y <= 50)
+                {
+                    ySpeed = -ySpeed;
+                }
+
+                // update position
+                pictureBox1.Location = new Point(pictureBox1.Location.X + xSpeed, pictureBox1.Location.Y + ySpeed);
+            };
+            moveTimer.Start();
+        }
+
+        // method that check the colliding with obstacle
+        private bool IsColliding(Control pictureBox, Panel obstacle)
+        {
+            Rectangle pictureBoxRect = pictureBox.Bounds;
+            Rectangle obstacleRect = obstacle.Bounds;
+
+            return pictureBoxRect.IntersectsWith(obstacleRect);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+                private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        // open second form
+        private void openButton_Click(object sender, EventArgs e)
+        {
+            Form1 secondForm = new Form1();
+            secondForm.Show();
+        }
+    }
+}
